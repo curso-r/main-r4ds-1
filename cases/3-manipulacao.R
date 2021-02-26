ef73a76e5965bc219d71a18a3104b979aaf1320a
williamorim <thewilliam89@gmail.com>
2021-02-26 14:41:45 -0300

    exemplos imdb

--- cases/3-manipulacao.R
+++ cases/3-manipulacao.R
@@ -23,6 +23,78 @@ tab_solicitada <- imdb %>%
 
 writexl::write_xlsx(tab_solicitada, "tab_filmes_ord_desc.xlsx")
 
+
+# -------------------------------------------------------------------------
+
+# Objetivo: descobrir qual o filme mais caro, 
+# mais lucrativo e com melhor nota dos anos 2000
+
+library(readxl)
+library(dplyr)
+
+imdb <- read_excel("dados/imdb.xlsx")
+
+## Filme mais caro ##
+
+# Vai retornar uma tabela de 0 linhas
+imdb %>% 
+  filter(ano %in% 2000:2009, orcamento == max(orcamento))
+
+# Usar na.rm = TRUE
+imdb %>% 
+  filter(
+    ano %in% 2000:2009, 
+    orcamento == max(orcamento, na.rm = TRUE)
+  ) %>% 
+  select(titulo, orcamento) %>% 
+  mutate(orcamento = orcamento / 1e6)
+
+# Por sorte dá certo, mas o certo seria
+imdb %>% 
+  filter(ano %in% 2000:2009) %>% 
+  filter(
+    orcamento == max(orcamento, na.rm = TRUE)
+  ) %>% 
+  select(titulo, orcamento) %>% 
+  mutate(orcamento = orcamento / 1e6)
+
+# Como vamos usar várias vezes a base filtrada, 
+# vamos criar a imdb_anos_2000
+
+imdb_anos_2000 <- imdb %>% 
+  filter(ano %in% 2000:2009)
+
+## Filme mais lucrativo ##
+imdb_anos_2000 %>% 
+  mutate(lucro = receita - orcamento) %>% 
+  filter(
+    lucro == max(lucro, na.rm = TRUE)
+  ) %>% 
+  select(titulo, lucro) %>% 
+  mutate(lucro = lucro / 1e6)
+
+# Melhor nota ##
+imdb_anos_2000 %>% 
+  filter(
+    nota_imdb == max(nota_imdb, na.rm = TRUE)
+  ) %>% 
+  select(titulo, nota_imdb)
+
+## Fazendo para todas as décadas ##
+
+imdb %>% 
+  dplyr::filter(!is.na(ano)) %>% 
+  mutate(
+    decada = floor(ano / 10) * 10
+  ) %>% 
+  group_by(decada) %>% 
+  filter(
+    nota_imdb == max(nota_imdb, na.rm = TRUE)
+  ) %>% 
+  select(titulo, nota_imdb) %>% 
+  arrange(decada)
+
+
 # -------------------------------------------------------------------------
 
 # Objetivo: descobrir qual o peso médio dos 
